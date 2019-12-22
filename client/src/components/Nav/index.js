8113600647c4cb6d2c71dee5f75bd60adf427cf6
YI YING <supercarboy88@gmail.com>
2019-12-21 23:59:56 -0800

    modified Nav index.js

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,12 +2,31 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-primary bg-primary">
       <a className="navbar-brand" href="/">
-        React Reading List
+        <h2 className="text-white">Google Books React Search </h2>
       </a>
+      <div id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item" id="home">
+            <a className="nav-link" href="/">
+              <button type="button" className="btn btn-info text-white">
+                Search
+              </button>
+            </a>
+          </li>
+          <li className="nav-item" id="report">
+            <a className="nav-link" href="/saved">
+              <button type="button" className="btn btn-warning text-white">
+                Saved
+              </button>
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
+
 
 export default Nav;
