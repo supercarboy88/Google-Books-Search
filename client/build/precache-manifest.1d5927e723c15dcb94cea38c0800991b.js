faca916cbaf1897d3318a23f00626a94e4a15dc1
YI YING <supercarboy88@gmail.com>
2019-12-23 03:12:35 -0800

    bulld folder

---
+++ client/build/precache-manifest.1d5927e723c15dcb94cea38c0800991b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cb2d42b9be05e3c040e01e55e4aa47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "327b4cb2c5b1730270c6",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "7639b8b1bbce47e7edbf",
+    "url": "/static/css/main.7e9b41fc.chunk.css"
+  },
+  {
+    "revision": "327b4cb2c5b1730270c6",
+    "url": "/static/js/2.cb97c58f.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.cb97c58f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7639b8b1bbce47e7edbf",
+    "url": "/static/js/main.ad11bd08.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "4fae52a299f7e69d997efb1cb6d6a939",
+    "url": "/static/media/books.4fae52a2.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks0");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:LDGMb!Xhyas2buk@ds257698.mlab.com:57698/heroku_zhxtwr1r");
 
 // Start the API server
 app.listen(PORT, function() {
